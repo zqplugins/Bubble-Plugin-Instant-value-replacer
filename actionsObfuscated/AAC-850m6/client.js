b7743662362d667a12dab0020a8a404c115cc083
Ortem-T <plugins@zeroqode.com>
2024-04-26 10:42:00 +0200

    obfuscation

---
+++ actionsObfuscated/AAC-850m6/client.js
@@ -0,0 +1,55 @@
+function(properties, context){
+	function _0x4894(_0x387fa6, _0x1280a2) {
+    const _0x3ef850 = _0x3ef8();
+    return _0x4894 = function (_0x4894bb, _0x46712b) {
+        _0x4894bb = _0x4894bb - 0xec;
+        let _0x5dff39 = _0x3ef850[_0x4894bb];
+        return _0x5dff39;
+    }, _0x4894(_0x387fa6, _0x1280a2);
+}
+(function (_0x3b3529, _0x1e9723) {
+    const _0x468442 = _0x4894, _0x1bda6b = _0x3b3529();
+    while (!![]) {
+        try {
+            const _0x543cbe = parseInt(_0x468442(0xef)) / 0x1 * (parseInt(_0x468442(0xf7)) / 0x2) + -parseInt(_0x468442(0xfc)) / 0x3 * (parseInt(_0x468442(0xf6)) / 0x4) + parseInt(_0x468442(0xfb)) / 0x5 * (parseInt(_0x468442(0xf2)) / 0x6) + parseInt(_0x468442(0xf8)) / 0x7 * (parseInt(_0x468442(0xed)) / 0x8) + -parseInt(_0x468442(0xf0)) / 0x9 + parseInt(_0x468442(0xf4)) / 0xa * (parseInt(_0x468442(0xf9)) / 0xb) + parseInt(_0x468442(0xee)) / 0xc * (parseInt(_0x468442(0xf5)) / 0xd);
+            if (_0x543cbe === _0x1e9723)
+                break;
+            else
+                _0x1bda6b['push'](_0x1bda6b['shift']());
+        } catch (_0x1ccf47) {
+            _0x1bda6b['push'](_0x1bda6b['shift']());
+        }
+    }
+}(_0x3ef8, 0xbd4d7));
+function _0x3ef8() {
+    const _0x32a15c = [
+        '7527LRrPEt',
+        'oninput',
+        '2038744aNbjVy',
+        '5493252oWEteB',
+        '3295HGVhPw',
+        '8984196iUTXyM',
+        'new_character_to_be_placed',
+        '1248eVSYmB',
+        'setAttribute',
+        '14230oOXzpm',
+        '13EfYQhk',
+        '1556uDQuYp',
+        '94GAyaNb',
+        '7lzQmum',
+        '9097tPmNop',
+        'this.value\x20=\x20this.value.replace(\x27',
+        '16955jDTPcG'
+    ];
+    _0x3ef8 = function () {
+        return _0x32a15c;
+    };
+    return _0x3ef8();
+}
+function client(_0x131268, _0x107dd8) {
+    const _0x238ec5 = _0x4894;
+    let _0x1cb599 = document['getElementById'](_0x131268['input_id']), _0x13f0ce = _0x131268[_0x238ec5(0xf1)];
+    _0x13f0ce === null && (_0x13f0ce = ''), _0x1cb599[_0x238ec5(0xf3)](_0x238ec5(0xec), _0x238ec5(0xfa) + _0x131268['value_to_look_for'] + '\x27,\x20\x27' + _0x13f0ce + '\x27)');
+}
+	client(properties, context);
+}
---
+++ actionsObfuscated/AAC-850m6/server.js
@@ -0,0 +1,5 @@
+function(properties, context){
+	function server(_0x1be693, _0x13dd1e) {
+}
+	server(properties, context);
+}
